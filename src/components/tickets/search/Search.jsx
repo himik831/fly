0a3d05bf97c05504510e3e046808d3fe3e5ec9dc
spataricristian <spataricristinel@gmail.com>
2022-07-13 20:26:55 +0300

    [modify] adjust ticket

--- src/components/tickets/search/Search.jsx
+++ src/components/tickets/search/Search.jsx
@@ -1,9 +1,24 @@
-import classes from "./Search.module.scss"
+import classes from "./Search.module.scss";
+import { MdOutlineAirplanemodeActive } from "react-icons/md";
 
 export default function Search() {
+  const stylePlane = { transform: "rotate(90deg)", margin: "0px 10px" };
+
   return (
     <div className={classes.body}>
-      <div className={classes.free}></div>
+      <div className={classes.free}>
+        <div className={classes.search}>
+          <div className={classes.information}>
+            <div className={classes.location}>
+              {"Chisinau"}
+              <MdOutlineAirplanemodeActive style={stylePlane} />
+              {"Rome"}
+            </div>
+            <div className={classes.date}>12.06.22 - 15.06.22 | 3 person</div>
+          </div>
+          <div className={classes.button}>Edit</div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/tickets/ticket/Ticket.jsx
+++ src/components/tickets/ticket/Ticket.jsx
@@ -1,6 +1,14 @@
 import classes from "./Ticket.module.scss";
-import { MdOutlineAirplanemodeActive } from "react-icons/md";
+import {
+  MdOutlineAirplanemodeActive,
+  MdAirlineSeatReclineExtra,
+} from "react-icons/md";
+import { BsFillPersonFill } from "react-icons/bs";
+import { BsHandbagFill } from "react-icons/bs";
 export default function Ticket() {
+  const styleDeparture = { transform: "rotate(45deg)" };
+  const styleDestination = { transform: "rotate(135deg)" };
+
   return (
     <div className={classes.body}>
       <div className={classes.content}>
@@ -15,21 +23,65 @@ export default function Ticket() {
             <div className={classes.departure}>
               <div className={classes.time}>11:25</div>
               <div className={classes.country}>Chisinau</div>
-              <div className={classes.date}>MON, 12.07.22</div>
+              <div className={classes.date}>12.07.22</div>
             </div>
             <div className={classes.flight_time}>
-              <div className={classes.time}></div>
-              <div className={classes.time_line}></div>
-              <div className={classes.code_iata}></div>
+              <div className={classes.time}>
+                <div className={classes.plane}>
+                  <MdOutlineAirplanemodeActive
+                    size={14}
+                    style={styleDeparture}
+                  />
+                </div>
+                <div className={classes.time_on_plane}>1h 53m</div>
+                <div className={classes.plane}>
+                  <MdOutlineAirplanemodeActive
+                    size={14}
+                    style={styleDestination}
+                  />
+                </div>
+              </div>
+              <div className={classes.time_line}>
+                <div className={classes.circle} />
+                <div className={classes.line} />
+                <div className={classes.circle} />
+              </div>
+              <div className={classes.code_iata}>
+                <div className={classes.code}>KIV</div>
+                <div className={classes.escale}>2 escale</div>
+                <div className={classes.code}>LTN</div>
+              </div>
             </div>
             <div className={classes.destination}>
               <div className={classes.time}>11:25</div>
               <div className={classes.country}>London</div>
-              <div className={classes.date}>MON, 12.07.22</div>
+              <div className={classes.date}>12.07.22</div>
             </div>
           </div>
-          <div className={classes.other}>other</div>
-          <div className={classes.details}>details</div>
+          <div className={classes.line_delimiter} />
+          <div className={classes.other}>
+            <div className={classes.bagage}>
+              <div className={classes.icon}>
+                <BsHandbagFill size={10} />
+              </div>
+              <div className={classes.text}>inclus</div>
+            </div>
+            <div className={classes.person}>
+              <div className={classes.icon}>
+                <BsFillPersonFill />
+              </div>
+              <div className={classes.text}>1 person</div>
+            </div>
+            <div className={classes.type}>
+              <div className={classes.icon}>
+                <MdAirlineSeatReclineExtra />
+              </div>
+              <div className={classes.text}>econom</div>
+            </div>
+          </div>
+          <div className={classes.details}>
+            <div className={classes.button}>Detalii</div>
+          </div>
         </div>
       </div>
     </div>
