9ada2c9a6f894ad6fb0772d849691e60b7ad8990
spataricristian <spataricristinel@gmail.com>
2022-07-15 16:44:50 +0300

    k

--- src/index.js
+++ src/index.js
@@ -10,10 +10,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/fly">
+      <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<App />} />
-          <Route path="/fly/tickets" element={<Tickets />} />
+          <Route path="/tickets" element={<Tickets />} />
           <Route path="*" element={<div>Not implemented</div>} />
         </Routes>
       </BrowserRouter>
