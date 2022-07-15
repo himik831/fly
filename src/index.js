37a87d6ace452ada60d9e80a511e9bdad0703ce6
spataricristian <spataricristinel@gmail.com>
2022-07-15 16:49:23 +0300

    test

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/">
+      <BrowserRouter basename="/fly">
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/tickets" element={<Tickets />} />
